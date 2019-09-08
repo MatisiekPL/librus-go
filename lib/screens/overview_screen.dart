aebe436807fb5a800785ee780962212bd1feca36
Mateusz <matisiek11@gmail.com>
2019-09-08 16:18:41 +0200

    :sparkles: Implemented nav drawer

--- lib/screens/overview_screen.dart
+++ lib/screens/overview_screen.dart
@@ -11,14 +11,50 @@ class _OverviewScreenState extends State<OverviewScreen> {
   void initState() {
     super.initState();
     Future.delayed(Duration.zero, () {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+//      Navigator.push(
+//          context, MaterialPageRoute(builder: (context) => LoginScreen()));
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Podsumowanie')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Mateusz Woźniak"),
+              accountEmail: Text("3656771u"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor:
+                    Theme.of(context).platform == TargetPlatform.iOS
+                        ? Colors.blue
+                        : Colors.white,
+                child: Text(
+                  "M",
+                  style: TextStyle(fontSize: 40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
       body: SafeArea(child: Center(child: CircularProgressIndicator())),
     );
   }
