c4f1b27297d5cc3f1a49b88839880acefbc7483f
Mateusz <matisiek11@gmail.com>
2019-09-09 16:40:39 +0200

    :sparkles: Filled grades fragment skeleton

--- lib/fragments/grades_fragment.dart
+++ lib/fragments/grades_fragment.dart
@@ -8,7 +8,9 @@ class GradesFragment extends StatefulWidget {
 class _GradesFragmentState extends State<GradesFragment> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+        itemCount: 25,
+        itemBuilder: (context, int subjectIndex) => SubjectWidget());
   }
 }
 
@@ -16,7 +18,25 @@ class _GradesFragmentState extends State<GradesFragment> {
 class SubjectWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 54.0, top: 20.0),
+          child: Container(
+            child: Text(
+              'Matematyka',
+              style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        ListView.builder(
+            shrinkWrap: true,
+            itemCount: 20,
+            physics: ClampingScrollPhysics(),
+            itemBuilder: (context, int gradeIndex) => GradeWidget())
+      ],
+    );
   }
 }
 
@@ -24,6 +44,36 @@ class SubjectWidget extends StatelessWidget {
 class GradeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Text(
+              '6+',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Aktywność',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  'Czwartek',
+                  style: TextStyle(fontSize: 16.0, fontStyle: FontStyle.italic),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
