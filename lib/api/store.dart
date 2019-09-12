120d57292d8ca1d030b13d892bdc1d5db866a76c
Mateusz <matisiek11@gmail.com>
2019-09-12 21:19:57 +0200

    :sparkles: Added librus tokens caching

--- lib/api/store.dart
+++ lib/api/store.dart
@@ -4,6 +4,7 @@ import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:html/parser.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Store {
   static BehaviorSubject fragmentSubject = new BehaviorSubject();
@@ -24,31 +25,21 @@ class Store {
     var response = await client.get(
         '$baseUrl/oauth2/authorize?client_id=$clientId&redirect_uri=http://localhost/bar&response_type=code');
     var document = parse(response.data);
-
-    // Get CSRF from HTML
-    var csrfToken = document
+    var csrf = document
         .querySelector('meta[name="csrf-token"][content]')
         .attributes['content'];
-
-    print(csrfToken);
-    // Authorize by POSTing credentials
+    print('Got CSRF: ' + csrf);
     await client.post('$baseUrl/rodzina/login/action',
         data: json.encode({'email': username, 'password': password}),
         options: Options(headers: {
-          'X-CSRF-TOKEN': csrfToken,
+          'X-CSRF-TOKEN': csrf,
           'Content-Type': "application/json"
         }));
-
-    // Get auth code by re-visiting the code URL
-    // It will now redirect to localhost with auth code supplied as a parameter.
     var codeResponse = await client.get(
         '$baseUrl/oauth2/authorize?client_id=$clientId&redirect_uri=http://localhost/bar&response_type=code',
         options: Options(
             followRedirects: false, validateStatus: (status) => status < 500));
-
     var authCode = codeResponse.headers.value('location').split('code=')[1];
-
-    // Exchange auth code for Librus account token
     var exchangeToken = await client.post(
       '$baseUrl/oauth2/access_token',
       data: {
@@ -59,10 +50,13 @@ class Store {
       },
       options: Options(headers: {"Content-Type": "application/json"}),
     );
-
-    var accessToken = exchangeToken.data['access_token'];
-    print(accessToken);
-
-    return accessToken;
+    var librusToken = exchangeToken.data['access_token'];
+    var librusRefreshToken = exchangeToken.data['refresh_token'];
+    print('Got Librus token: ' + librusToken);
+    print('Got Librus refresh token: ' + librusRefreshToken);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("librus_token", librusToken);
+    await prefs.setString("librus_refresh_token", librusRefreshToken);
+    return librusToken;
   }
 }
