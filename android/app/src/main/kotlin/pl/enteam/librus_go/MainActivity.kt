28aefbf128f317dd3455f1b54ea804b2c804bcd8
Mateusz <matisiek11@gmail.com>
2019-11-15 15:21:49 +0100

    :bug: Added screen for testing robots

--- android/app/src/main/kotlin/pl/enteam/librus_go/MainActivity.kt
+++ android/app/src/main/kotlin/pl/enteam/librus_go/MainActivity.kt
@@ -1,13 +1,25 @@
 package pl.enteam.librus_go
 
 import android.os.Bundle
+import android.provider.Settings
 
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+  private val CHANNEL = "librus_go.enteam.pl/utils"
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+
+    MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
+      if(call.method == "checkIfInAutomatedTestsEnvironment") {
+        val isInAutomatedTestsEnvironment = "true" == Settings.System.getString(contentResolver, "firebase.test.lab")
+        result.success(isInAutomatedTestsEnvironment)
+      }
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:connectivity/connectivity.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:librus_go/screens/login_screen.dart';
 import 'package:librus_go/screens/offline_screen.dart';
@@ -10,8 +11,21 @@ import 'api/store.dart';
 
 FirebaseAnalytics analytics = FirebaseAnalytics();
 
+const platform = const MethodChannel('librus_go.enteam.pl/utils');
+
+Future<bool> checkIfRunningInAutomatedTestsEnvironment() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  return await platform.invokeMethod("checkIfInAutomatedTestsEnvironment") as bool;
+}
+
 void main() {
-  initializeDateFormatting().then((_) {
+  initializeDateFormatting()
+      .then((_) => checkIfRunningInAutomatedTestsEnvironment())
+      .then((result) {
+        if(result) {
+          runApp(StopRobot());
+          return;
+        }
     Store.init();
     runApp(App());
   });
@@ -42,6 +56,20 @@ class App extends StatelessWidget {
                     body: Center(child: new CircularProgressIndicator()),
                   ),
       ),
+    );
+  }
+}
+
+class StopRobot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Librus Go",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(body: Center(child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Text('Przepraszamy, aplikacja nie jest dostępna dla robotów testujących', textAlign: TextAlign.center,),
+      )),),
     );
   }
 }
